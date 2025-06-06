06b0b0a7b2fa02b0213d0e4c67af5ff6de9b5338
stivenFory <stiven.fory@siesa.com>
2025-06-06 11:14:58 -0500

    Primer lanzamiento

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="TercerRepo",                           # Nombre del paquete
+    version="0.1.0",                                # Versión inicial
+    packages=find_packages(),                       # Paquetes a incluir
+    description="Un paquete pip simple de saludo",  # Breve descripción
+    author="Stiven Fory",                         # Tu nombre
+    author_email="stiven.fory@gmail.com",                 # Tu correo electrónico
+    url="https://github.com/stivenfory/tercerrepo",     # URL del proyecto
+)
